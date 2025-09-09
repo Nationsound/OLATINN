b3b033d0943e0d4327194b7ed1d05fd974d18577
Nationsound <bookingnationsound@gmail.com>
2025-09-09 16:46:15 +0100

    Blog Service and Designs coming soon

---
+++ src/app/service/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="p-8">
+      <h1 className="text-4xl font-bold">Our Service</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Welcome to the Service section. Posts coming soon!
+      </p>
+    </main>
+  )
+}
+
+export default page
